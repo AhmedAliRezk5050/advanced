da2b0890c5e665725c6c1ba6d3c8699c624e17a9
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-10 14:33:29 +0200

    configurations - add bootstrap

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using Advanced.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<DataContext>(opts => {
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
     opts.EnableSensitiveDataLogging(true);
@@ -10,7 +14,15 @@ builder.Services.AddDbContext<DataContext>(opts => {
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.MapControllerRoute(
+    "controllers",
+    "controllers/{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 var context = app.Services.CreateScope()
                 .ServiceProvider
