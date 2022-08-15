4c1d646d317ab197243879501a6086ecffaf0850
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-15 06:29:20 +0200

    Identity - Validating Passwords

--- Advanced/Program.cs
+++ Advanced/Program.cs
@@ -25,6 +25,15 @@ builder.Services.AddDbContext<IdentityContext>(opts =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<IdentityContext>();
 
+builder.Services.Configure<IdentityOptions>(opts =>
+{
+    opts.Password.RequiredLength = 6;
+    opts.Password.RequireNonAlphanumeric = false;
+    opts.Password.RequireLowercase = false;
+    opts.Password.RequireUppercase = false;
+    opts.Password.RequireDigit = false;
+});
+
 var app = builder.Build();
 
 app.UseStaticFiles();
