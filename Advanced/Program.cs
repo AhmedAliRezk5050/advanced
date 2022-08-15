499f27c89a0374790fcb0dc8598183d81436c2ef
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-15 04:14:09 +0200

    Identity - configurations

--- Advanced/Program.cs
+++ Advanced/Program.cs
@@ -1,23 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Advanced.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages();
-  
+
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddDbContext<DataContext>(opts => {
+builder.Services.AddDbContext<DataContext>(opts =>
+{
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
     opts.EnableSensitiveDataLogging(true);
 });
 
 builder.Services.AddSingleton<Advanced.Services.ToggleService>();
 
+builder.Services.AddDbContext<IdentityContext>(opts =>
+    opts.UseSqlServer(builder.Configuration[
+        "ConnectionStrings:IdentityConnection"]));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>();
+
 var app = builder.Build();
- 
+
 app.UseStaticFiles();
 
 app.MapControllers();
@@ -37,8 +46,8 @@ app.UseBlazorFrameworkFiles("/webassembly");
 app.MapFallbackToFile("/webassembly/{*path:nonfile}", "/webassembly/index.html");
 
 var context = app.Services.CreateScope()
-                .ServiceProvider
-                .GetRequiredService<DataContext>();
+    .ServiceProvider
+    .GetRequiredService<DataContext>();
 
 SeedData.SeedDatabase(context);
 
